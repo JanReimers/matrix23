6e0484b8bd7470100a4766fcbb192fcfa7c5d5a0
Jan Reimers <janreimers@gmail.com>
2025-06-27 08:44:49 -0600

    Start testing Chapers

--- unittests/main.cpp
+++ unittests/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
-
+    GTEST_FLAG_SET(death_test_style, "fast"); //Assume single threads in test for now.
     return RUN_ALL_TESTS();
 }
 
